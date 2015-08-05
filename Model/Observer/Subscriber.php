a26c1219d0e3cd8b55eca7fd5189494737fdfb23
Lucas Guarnieri <lucas.guarnieri.s@gmail.com>
2015-08-05 18:38:20 -0300

    Adiciona campos para as merges tags de origem e sobrenome

--- Model/Observer/Subscriber.php
+++ Model/Observer/Subscriber.php
@@ -24,8 +24,13 @@ class Cammino_Mailchimp_Model_Observer_Subscriber extends Varien_Object
 
                 $token = Mage::getStoreConfig("newsletter/mailchimp/token");
                 $list = Mage::getStoreConfig("newsletter/mailchimp/list_id");
+                
                 $nameParam = Mage::getStoreConfig("newsletter/mailchimp/name_param");
                 $nameMergeVar = Mage::getStoreConfig("newsletter/mailchimp/name_merge_var");
+                $lastNameMergeVar = Mage::getStoreConfig("newsletter/mailchimp/last_name_merge_var");
+
+                $originParam = Mage::getStoreConfig("newsletter/mailchimp/origin_param");
+                $originMergeVar = Mage::getStoreConfig("newsletter/mailchimp/origin_merge_var");
 
                 $groupIdParam = Mage::getStoreConfig("newsletter/mailchimp/group_id_param");
                 $interestGroupParam = Mage::getStoreConfig("newsletter/mailchimp/interest_group_param");
@@ -33,8 +38,9 @@ class Cammino_Mailchimp_Model_Observer_Subscriber extends Varien_Object
                 $defaultGroupId = Mage::getStoreConfig("newsletter/mailchimp/default_group_id");
                 $defaultInterestGroup = Mage::getStoreConfig("newsletter/mailchimp/default_interest_group");
 
-                $email = $subscriber->getEmail();
-                $name = isset($params[$nameParam]) ? $params[$nameParam] : "";
+                $email  = $subscriber->getEmail();
+                $name   = isset($params[$nameParam]) ? ( !empty($lastNameMergeVar) ? $this->getName($params[$nameParam]) : $params[$nameParam] ) : "";
+                $origin = isset($params[$originParam]) ? $params[$originParam] : "";
                 
                 $groupId = isset($params[$groupIdParam]) ? $params[$groupIdParam] : $defaultGroupId;
                 $interestGroup = isset($params[$interestGroupParam]) ? $params[$interestGroupParam] : $defaultInterestGroup;
@@ -42,8 +48,14 @@ class Cammino_Mailchimp_Model_Observer_Subscriber extends Varien_Object
                 $mailchimp = new MailChimp($token);
                 $mailchimp->set_timeout(3);
 
-                if (!empty($nameMergeVar) && !empty($name))
-                    $mergeVars[$nameMergeVar] = $name;
+                if (!empty($nameMergeVar) && !empty($name)){
+                    if ( is_array($name) ) {
+                        $mergeVars[$nameMergeVar]     = $name['firstName'];
+                        $mergeVars[$lastNameMergeVar] = $name['lastName'];
+                    } else {
+                        $mergeVars[$nameMergeVar] = $name;
+                    }
+                }
 
                 if (!empty($groupId))
                     $mergeVars["GROUPINGS"] = array(array( 'id' => $groupId ));
@@ -64,6 +76,9 @@ class Cammino_Mailchimp_Model_Observer_Subscriber extends Varien_Object
 
                     if ((strval($callResult["name"]) == "Email_NotExists") || (strval($callResult["name"]) == "List_NotSubscribed")) {
 
+                        if (!empty($originMergeVar) && !empty($origin))
+                            $mergeVars[$originMergeVar] = $origin;
+
                         $request = array(
                             'id' => $list,
                             'email' => array('email' => $email ),
@@ -78,8 +93,22 @@ class Cammino_Mailchimp_Model_Observer_Subscriber extends Varien_Object
                 }
 
             }
-
         } catch (Exception $e) {
+            Mage::log($e, null, 'subscriber.log');
         }
+    }
+
+    private function getName($fullName)
+    {        
+        if(!$fullName) return '';
+
+        $name = explode(' ', $fullName);
+        $lastName = '';
+
+        for ($i=1; $i < count($name); $i++) { 
+            $lastName .= $name[$i].' ';
+        }
+
+        return array( 'firstName' => $name[0], 'lastName' => $lastName );
     }
 }
