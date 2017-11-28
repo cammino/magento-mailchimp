0eab5cc0ed7f4fe434333a579c7ff9de9892961f
André Regino <andre_regino@hotmail.com>
2017-11-28 15:18:02 -0200

    Trata presença do https na chamada do controller via bloco

--- Block/Ecommerce360.php
+++ Block/Ecommerce360.php
@@ -9,10 +9,11 @@ class Cammino_Mailchimp_Block_Ecommerce360 extends Mage_Core_Block_Template {
 
 	protected function _toHtml() {
 		$html = "";
-
+		Mage::app()->getStore()->isCurrentlySecure();
 		if (strval($this->_enabled) == "1") {
 			$html .= '<script type="text/javascript">
-				new Ajax.Request("'.Mage::getUrl('mailchimp/ecommerce360/send').'",{ method:"get", onComplete: function (data) { } });
+				new Ajax.Request("'.Mage::getUrl('mailchimp/ecommerce360/send', array(
+    			'_secure' => true)).'",{ method:"get", onComplete: function (data) { } });
 			</script>';
 		}
 
